b0c9f39e60beb33a104187bc554a4e8b025112db
Lucas Livchits <livchitsl@gmail.com>
2021-06-04 16:56:36 -0300

    Add more styles to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,15 +4,22 @@ function Form() {
   return (
     <form className='my-8 text-center'>
       <div className='mx-auto w-5/6 text-left'>
-        <label className='ml-2 text-indigo-300 ' htmlFor='csv-url'>Ingrese la url del archivo</label>
+        <label className='ml-2 text-indigo-300 ' htmlFor='csv-url'>
+          Ingrese la url del archivo
+        </label>
         <input
-          className='bg-indigo-900 border-2 border-emerald-500 p-1.5 rounded-lg leading-relaxed text-indigo-200 mt-1 w-full focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent focus:ring-offset-1 focus:ring-offset-transparent'
+          className='p-1.5 mt-1 w-full leading-relaxed text-indigo-200 bg-indigo-900 rounded-lg border-2 border-emerald-500 focus:border-transparent focus:ring-2 focus:ring-emerald-600 focus:ring-offset-1 focus:ring-offset-transparent focus:outline-none'
           id='csv-url'
           name='csv-url'
           type='text'
         />
       </div>
-      <button className='bg-emerald-500 text-indigo-50 font-bold py-4 px-8 rounded-full my-6 text-xl text-center hover:bg-emerald-600 hover:text-indigo-100 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-indigo-900 focus:ring-offset-2' type='submit'>Generar agenda</button>
+      <button
+        className='py-4 px-8 my-6 text-xl font-bold text-center text-indigo-50 hover:text-indigo-100 bg-emerald-500 hover:bg-emerald-600 rounded-full focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 focus:ring-offset-indigo-900 transition-colors duration-200 focus:outline-none'
+        type='submit'
+      >
+        Generar agenda
+      </button>
     </form>
   );
 }

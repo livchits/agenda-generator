95d6438cde79aadf9bf91f272b0bccf71a165fa4
Lucas Livchits <livchitsl@gmail.com>
2021-06-04 19:51:39 -0300

    Add mrgin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function Footer() {
   return (
-    <footer className='py-2 mx-auto mb-2 w-3/4 text-center text-indigo-100 bg-indigo-800 bg-opacity-95 rounded-2xl'>
+    <footer className='py-2 my-2 mx-auto w-3/4 text-center text-indigo-100 bg-indigo-800 bg-opacity-95 rounded-2xl'>
       Creado por{' '}
       <a className='hover:text-indigo-300' href='https://github.com/livchits'>
         Lucas Livchits

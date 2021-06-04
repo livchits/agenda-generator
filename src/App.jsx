833aaca859c0b53e78a57791931cf9120c8c6a00
Lucas Livchits <livchitsl@gmail.com>
2021-06-04 17:04:45 -0300

    Add styles for largers screens

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
     <Container>
       <main>
         <section className='px-2 text-center'>
-          <h1 className='py-8 mt-2 text-4xl font-black text-white'>
+          <h1 className='py-8 my-2 text-4xl sm:text-5xl font-black text-white'>
             Generador de agenda
           </h1>
-          <p className='text-lg leading-snug text-indigo-200'>
+          <p className='mx-auto max-w-md text-lg leading-snug text-indigo-200'>
             Obtenga la agenda en dos versiones: texto con formato y para enviar por
             Whatsapp
           </p>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ function Form() {
     <form className='my-8 text-center'>
       <div className='mx-auto w-5/6 text-left'>
         <label className='ml-2 text-indigo-300 ' htmlFor='csv-url'>
-          Ingrese la url del archivo
+          Ingrese la url del archivo .csv
         </label>
         <input
           className='p-1.5 mt-1 w-full leading-relaxed text-indigo-200 bg-indigo-900 rounded-lg border-2 border-emerald-500 focus:border-transparent focus:ring-2 focus:ring-emerald-600 focus:ring-offset-1 focus:ring-offset-transparent focus:outline-none'

875b24d23856a90858fdaaa5916a0bd8f7ee1813
Lucas Livchits <livchitsl@gmail.com>
2021-06-04 17:54:36 -0300

    Add function to handle errors from papaparse

--- src/utils/parseCSV.jsx
+++ src/utils/parseCSV.jsx
@@ -2,17 +2,23 @@ import Papa from 'papaparse';
 
 import sanitizeObject from './sanitizeObject';
 
-function parseCSV(csvUrl, setState) {
+function parseCSV(csvUrl, setSchedule) {
   Papa.parse(csvUrl, {
     download: true,
     header: true,
     dinamicTyping: true,
     skipEmptyLines: true,
     worker: true,
+    error: (error) =>
+      setSchedule((previousSchedule) => ({
+        ...previousSchedule,
+        status: 'failed',
+        error,
+      })),
     complete: ({ data }) => {
-      setState((previousSchedule) => ({ ...previousSchedule, status: 'pending' }));
+      setSchedule((previousSchedule) => ({ ...previousSchedule, status: 'pending' }));
       const sanitizedData = data.map(sanitizeObject);
-      setState((previousSchedule) => ({
+      setSchedule((previousSchedule) => ({
         ...previousSchedule,
         status: 'complete',
         data: sanitizedData,

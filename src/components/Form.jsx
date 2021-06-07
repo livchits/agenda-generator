a0043eeb39d16143bbf4f4b42a96ddc8d8189de4
Lucas Livchits <livchitsl@gmail.com>
2021-06-07 17:29:16 -0300

    Add checkbox and state for custom url

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as Rolling } from '../assets/rolling.svg';
 import parseCSV from '../utils/parseCSV';
 
 function Form({ setSchedule, status }) {
+  const [enableCustomUrl, setEnableCustomUrl] = React.useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const { value: csvUrl } = event.target.elements['csv-url'];
@@ -12,15 +14,30 @@ function Form({ setSchedule, status }) {
     parseCSV(csvUrl, setSchedule);
   };
 
+  const handleCheckBoxChange = () => {
+    setEnableCustomUrl((enableCustomUrl) => !enableCustomUrl);
+  };
+
   return (
     <form className='my-8 text-center' onSubmit={handleSubmit}>
       <div className='mx-auto w-5/6 text-left'>
-        <label className='ml-2 text-indigo-300 ' htmlFor='csv-url'>
-          Ingrese la url del archivo .csv
+        <label className='ml-2 text-indigo-300' htmlFor='custom-url'>
+          Usar url personalizada para el archivo .csv
         </label>
         <input
+          checked={enableCustomUrl}
+          id='custom-url'
+          name='custom-url'
+          type='checkbox'
+          onChange={handleCheckBoxChange}
+        />
+        <input
           required
-          className='p-1.5 mt-1 w-full leading-relaxed text-indigo-200 bg-indigo-900 rounded-lg border-2 border-emerald-500 focus:border-transparent focus:ring-2 focus:ring-emerald-600 focus:ring-offset-1 focus:ring-offset-transparent focus:outline-none'
+          className={`p-1.5 mt-1 w-full leading-relaxed text-indigo-200  rounded-lg border-2  focus:border-transparent focus:ring-2 focus:ring-emerald-600 focus:ring-offset-1 focus:ring-offset-transparent focus:outline-none ${enableCustomUrl
+              ? 'bg-indigo-900 border-emerald-500'
+              : 'bg-gray-400 border-gray-400'
+            }`}
+          disabled={!enableCustomUrl}
           id='csv-url'
           name='csv-url'
           type='text'

fa9b651998d0c5412f6ff84c222259223a89916d
Lucas Livchits <livchitsl@gmail.com>
2021-06-07 23:29:27 -0300

    Add sheet url to constants and .env

--- src/utils/getGoogleSheetUrl.js
+++ src/utils/getGoogleSheetUrl.js
@@ -1,5 +1,7 @@
+import { URL, SUFIX } from '../constants';
+
 function getGoogleSheetUrl(id) {
-  return `https://docs.google.com/spreadsheets/d/e/2PACX-1vTShK5eoDPRlxQHoT8bX6cGSkvS_Ip6iZqsqNtJAU36VOyl1Y2vA7-ARFD7lsd1wYj0rd0DVmuFpdXc/pub?gid=${id}&single=true&output=csv`;
+  return `${URL}${id}${SUFIX}`;
 }
 
 export default getGoogleSheetUrl;

fa9eabb167a5dd011a9abe05c28764e4dcee6d7c
Lucas Livchits <livchitsl@gmail.com>
2021-06-07 23:22:14 -0300

    Add styles for checkbox

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,16 +29,33 @@ function Form({ setSchedule, status }) {
   return (
     <form className='my-8 text-center' onSubmit={handleSubmit}>
       <div className='mx-auto w-5/6 text-left'>
-        <label className='ml-2 text-indigo-300' htmlFor='custom-url'>
-          Usar url personalizada para el archivo .csv
-        </label>
-        <input
-          checked={enableCustomUrl}
-          id='custom-url'
-          name='custom-url'
-          type='checkbox'
-          onChange={handleCheckBoxChange}
-        />
+        <div className='flex relative items-center mr-4 mb-2 w-max'>
+          <label className='mx-2 text-indigo-300' htmlFor='custom-url'>
+            Usar url personalizada para el archivo .csv
+          </label>
+          <input
+            checked={enableCustomUrl}
+            className='absolute right-1 w-8 h-8 opacity-0'
+            id='custom-url'
+            name='custom-url'
+            type='checkbox'
+            onChange={handleCheckBoxChange}
+          />
+          <div className='flex flex-shrink-0 justify-center items-center mr-2 w-8 h-8 text-emerald-600 bg-indigo-100 rounded-md border-2 border-emerald-400 focus-within:border-emerald-600'>
+            <svg
+              className='hidden w-4 h-4 pointer-events-none'
+              version='1.1'
+              viewBox='0 0 17 12'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <g fill='none' fillRule='evenodd'>
+                <g fill='#059669' fillRule='nonzero' transform='translate(-9 -11)'>
+                  <path d='m25.576 11.414c0.56558 0.55188 0.56558 1.4439 0 1.9961l-9.404 9.176c-0.28213 0.27529-0.65247 0.41385-1.0228 0.41385-0.37034 0-0.74068-0.13855-1.0228-0.41385l-4.7019-4.588c-0.56584-0.55188-0.56584-1.4442 0-1.9961 0.56558-0.55214 1.4798-0.55214 2.0456 0l3.679 3.5899 8.3812-8.1779c0.56558-0.55214 1.4798-0.55214 2.0456 0z' />
+                </g>
+              </g>
+            </svg>
+          </div>
+        </div>
         <input
           required
           className={`${inputStyle} p-1.5 mt-1 w-full leading-relaxed text-indigo-200  rounded-lg border-2  focus:border-transparent focus:ring-2 focus:ring-emerald-600 focus:ring-offset-1 focus:ring-offset-transparent focus:outline-none`}

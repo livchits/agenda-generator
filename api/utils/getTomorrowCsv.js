bb921088890568a8e7d73fd8ae7e52995554656c
Lucas Livchits <livchitsl@gmail.com>
2021-08-27 16:19:31 -0300

    Change function to retrieve monday on friday, saturday and sunday

--- api/utils/getTomorrowCsv.js
+++ api/utils/getTomorrowCsv.js
@@ -1,9 +1,11 @@
 const { DAYS } = require('../constants');
+
 const { default: getGoogleSheetUrl } = require('./getGoogleSheetUrl');
 
 function getTomorrowCsv() {
   const day = new Date().getDay();
-  const csvUrl = getGoogleSheetUrl(DAYS[day + 1]);
+  const dayToGet = day >= 0 && day <= 4 ? day : 0;
+  const csvUrl = getGoogleSheetUrl(DAYS[dayToGet + 1]);
   return csvUrl;
 }
 
